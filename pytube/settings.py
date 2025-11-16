733b2dc5114eacb7398a3b78e289d7c6dfa497d7
Alok Kumar <alokk4891@gmail.com>
2025-11-16 14:06:31 +0530

    ALLOWED_HOSTS = ['*']

--- pytube/settings.py
+++ pytube/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-&da@3x1-l15ow$(ro)-13m_#=4(j1a#dnd7m35yh5whyfs=s8x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
